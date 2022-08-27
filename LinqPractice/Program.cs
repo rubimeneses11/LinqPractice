687fd77ff3169e82bc68bf3342635cd6c158a2e5
Rubi Meneses <rubi.ventura@icloud.com>
2022-08-27 15:34:54 -0500

    first commit

---
+++ LinqPractice/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+//Create a list of video game names.
+var gameName = new List<string> { "Fifa23", "Madden", "Red Dead Redemption", "Super Mario", "Tetris", "Overwatch", "Minecraft", "Fortnite", "League of Legends", "Grand Theft Auto V" };
+
+//Order the list of games by length of the game name.
+//Use the lambda expression in this exercise as well.
+gameName.OrderBy(x => x.Length).ToList().ForEach(x => Console.WriteLine(x));
